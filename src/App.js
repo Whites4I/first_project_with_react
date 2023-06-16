e3b2402e133515e5d84253d6df3d13877c36b531
Whites4I <andriypetrina2000@gmail.com>
2023-06-16 21:31:55 +0300

    Create new form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
-import classes from "./styles/App.css";
+import React, { useState } from "react";
+import "./styles/App.css";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
-import FormLogIn from "./components/FormLogIn";
+
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript', body: 'Description'},
+    {id: 3, title: 'Javascript', body: 'Description'},
+  ]);
+
+const addNewPost = () => {
+  
+}
+
   return (
-    <div className="App">
-          
+    <div className="App"> 
+    <form>
+      <MyInput type="text" placeholder="Name post"/>
+      <MyInput type="text" placeholder="Description post"/>
+      <MyButton onClick={addNewPost}>Create post</MyButton>
+    </form>
+      <PostList posts={posts} title='list posts 1'/>
     </div>
   );
 }
