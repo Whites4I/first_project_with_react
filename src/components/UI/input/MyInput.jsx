ebdae41beb9d8099f5a33ea89deab1ee37ffe2f6
Whites4I <andriypetrina2000@gmail.com>
2023-06-20 19:54:26 +0300

    Fix

--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import classes from "./MyInput.module.css"
 
-const MyInput = React.forwardRef((props, ref) => {
+const MyInput = React.forwardRef((props) => {
     return(
-        <input ref={ref} className={classes.myInput} {...props}></input>
+        <input className={classes.myInput} {...props}></input>
     );
 });
 
