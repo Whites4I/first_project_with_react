65589fe9b64153db728fe4e36086fe6dbeedce43
Whites4I <andriypetrina2000@gmail.com>
2023-06-21 23:13:53 +0300

    add hook useMemo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript", body: "Description" },
-    { id: 2, title: "Javascript", body: "Description" },
-    { id: 3, title: "Javascript", body: "Description" },
+    { id: 1, title: "a", body: "e" },
+    { id: 2, title: "v", body: "w" },
+    { id: 3, title: "s", body: "a" },
   ]);
 
   const [selectedSort, setSelectedSort] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const sortedPosts = useMemo(() => {
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort])
+      );
+    }
+    return posts;
+  }, [selectedSort, posts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -23,13 +34,17 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   };
 
   return (
     <div className='App'>
       <PostForm create={createPost} />
-
+      <hr style={{ margin: "15px 0" }} />
+      <MyInput
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder='Search...'
+      />
       <MySelect
         value={selectedSort}
         onChange={sortPosts}
@@ -41,7 +56,11 @@ function App() {
       />
 
       {posts.length ? (
-        <PostList remove={removePost} posts={posts} title='Posts for JS' />
+        <PostList
+          remove={removePost}
+          posts={sortedPosts}
+          title='Posts for JS'
+        />
       ) : (
         <h1 style={{ textAlign: "center" }}>Posts not find</h1>
       )}
