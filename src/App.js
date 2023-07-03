304f68f6e25abf6d796c4ede711b317f800b5ae2
Whites4I <andriypetrina2000@gmail.com>
2023-07-03 20:50:10 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -9,21 +9,29 @@ import usePosts from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import useFetching from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./components/utils/pages";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [modal, setModal] = useState(false);
   const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
 
+  let pagesArray = getPagesArray(totalPages);
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
   });
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [page]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -32,6 +40,10 @@ function App() {
 
   const removePost = (post) => {
     setPosts(posts.filter((i) => i.id !== post.id));
+  };
+
+  const changePage = (page) => {
+    setPage(page);
   };
 
   return (
@@ -65,6 +77,17 @@ function App() {
           title='Posts for JS'
         />
       )}
+      <div className='page__wrapper'>
+        {pagesArray.map((p) => (
+          <span
+            onClick={() => setPage(p)}
+            key={p}
+            className={page === p ? "page page__current" : "page"}
+          >
+            {p}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
