759630e182032d521232ab9cd96fe667bc8aac35
Whites4I <andriypetrina2000@gmail.com>
2023-07-11 16:50:49 +0300

    fix

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -10,9 +10,10 @@ const Navbar = () => {
     setIsAuth(false);
     localStorage.removeItem("auth");
   };
+
   return (
     <div className='navbar'>
-      <MyButton onClick={() => logout}>Exit</MyButton>
+      <MyButton onClick={() => logout()}>Exit</MyButton>
       <div className='navbar__links'>
         <Link to='about'>Info</Link>
         <Link to='posts'>Posts</Link>
